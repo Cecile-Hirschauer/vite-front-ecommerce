c647424986a41d2bf322ccd16419f47f932ee97b
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2024-11-08 17:05:30 +0100

    feat(list-products): create product item card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Header from './common/layout/Header';
+import ProductItem from './features/product/list-products/ui/ProductItem';
 
 function App() {
   const onSubmit = () => alert('Search');
 
   const cartCount = 2;
 
-  return <Header onSubmit={onSubmit} cartCount={cartCount} />;
+  const product = {
+    name: 'Product Test',
+    picture: '',
+    price: 10,
+  };
+
+  return (
+    <>
+      <Header onSubmit={onSubmit} cartCount={cartCount} />
+      <ProductItem product={product} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/features/product/list-products/ui/ProductItem/ProductItem.tsx
@@ -0,0 +1,39 @@
+import { Panorama } from '@mui/icons-material';
+import type { ReactElement } from 'react';
+
+type Props = {
+  product: {
+    name: string;
+    picture: string;
+    price: number;
+  };
+};
+
+const ProductItem = ({ product }: Props): ReactElement => {
+  return (
+    <div className="flex flex-col items-center justify-center w-64 p-4 m-4 bg-white rounded shadow-md">
+      <div className="w-48 h-48 shrink-0">
+        {product.picture && (
+          <img
+            className="size-full object-contain"
+            src={product.picture}
+            alt={product.name}
+          />
+        )}
+        {!product.picture && (
+          <div className="size-full bg-orange-100 flex items-center justify-center">
+            <Panorama fontSize="large" color="disabled" />
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col items-center justify-center w-full mt-4">
+        <h3 className="text-lg font-semibold text-center">{product.name}</h3>
+        <p className="mt-1 text-lg font-semibold text-center">
+          {product.price} €
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
